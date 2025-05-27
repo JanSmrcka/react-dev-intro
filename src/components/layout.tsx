85ad6e4f1e7e92aa6d1731db79e9da79d8f1ef1b
Tomas Jurica <tomas.jurica@icewarp.com>
2025-05-27 13:58:31 +0200

    router

---
+++ src/components/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode
+}
+
+export const Layout = ({children}: Props) => {
+    return (
+        <div className="container">{children}</div>
+    )
+}
