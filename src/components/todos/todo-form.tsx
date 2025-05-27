d4486b154eb95ac9d825c94568c1d01ec2d9f856
adamhanus <kecupwik@gmail.com>
2025-05-27 10:56:11 +0200

    Refactor TodoForm to accept addTodo as a prop and streamline form submission

--- src/components/todos/todo-form.tsx
+++ src/components/todos/todo-form.tsx
@@ -1,25 +1,24 @@
 import { useState, type ChangeEvent } from 'react'
 
-export const TodoForm = () => {
+type TodoFormProps = {
+  addTodo: (todoName: string) => void
+}
+
+export const TodoForm = ({ addTodo }: TodoFormProps) => {
   const [todoName, setTodoName] = useState('') // Pole - na prvni posici je hodnota stavu, na druhe funkce
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log('Input changed: ', e.target.value)
     setTodoName(e.target.value)
   }
 
   const handleSubmit = () => {
-    console.log('Form submited')
+    addTodo(todoName)
   }
 
   return (
-    <form>
-      <div className="input-group">
-        <input value={todoName} onChange={handleInputChange} name="todo-text" placeholder="What needs to be done?" />
-        <button onClick={handleSubmit} type="submit">
-          Add
-        </button>
-      </div>
-    </form>
+    <div className="input-group">
+      <input value={todoName} onChange={handleInputChange} name="todo-text" placeholder="What needs to be done?" />
+      <button onClick={handleSubmit}>Add</button>
+    </div>
   )
 }
