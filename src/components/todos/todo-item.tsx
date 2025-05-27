00c01b529fe98d9a0ecc9e3d0711b779245b601b
adamhanus <kecupwik@gmail.com>
2025-05-27 11:17:49 +0200

    Add toggle functionality to TodoItem and TodosSection components

--- src/components/todos/todo-item.tsx
+++ src/components/todos/todo-item.tsx
@@ -3,17 +3,25 @@ import type { Todo } from '../../types'
 type TodoItemProps = {
   todo: Todo
   deleteTodo: (todoId: number) => void
+  toggleTodo: (todoId: number, completed: boolean) => void
 }
 
-export const TodoItem = ({ todo, deleteTodo }: TodoItemProps) => {
+export const TodoItem = ({ todo, deleteTodo, toggleTodo }: TodoItemProps) => {
   const handleDeleteTodo = () => {
     deleteTodo(todo.id)
   }
 
+  const handleToggleTodo = () => {
+    toggleTodo(todo.id, todo.completed)
+  }
+
   return (
-    <li>
+    <li className={todo.completed ? 'completed' : ''}>
       <span>{todo.name}</span>
       <button onClick={handleDeleteTodo}>Delete</button>
+      <button onClick={handleToggleTodo} className="toggle">
+        {todo.completed ? 'Undo' : 'Completed'}
+      </button>
     </li>
   )
 }
