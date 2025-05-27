ad19decc79916836198b0b947010847101d95471
Petr Stipek <stipepet@fit.cvut.cz>
2025-05-27 09:39:40 +0200

    todos section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import { Header } from "./components/header"
+import { TodosSection } from "./components/todos/todos-section"
 
 function App() {
   return (
     <>
       <div className="container">
         <Header title="My Todo List" subtitle="Add your tasks" />
-        <main>
-          <form id="todo-form">
-            <div className="input-group">
-              <input name="todo-text" id="new-todo-input" placeholder="What needs to be done?" />
-              <button type="submit" id="add-btn">
-                Add
-              </button>
-            </div>
-          </form>
-          <div className="todo-container">
-            <ul id="todo-list"></ul>
-          </div>
-        </main>
+        <TodosSection />
         <footer>
           <p>Click on a task to mark it as completed</p>
         </footer>
---
+++ src/components/todos/todos-section.tsx
@@ -0,0 +1,17 @@
+export const TodosSection = () => {
+    return (
+        <main>
+            <form id="todo-form">
+                <div className="input-group">
+                    <input name="todo-text" id="new-todo-input" placeholder="What needs to be done?" />
+                    <button type="submit" id="add-btn">
+                        Add
+                    </button>
+                </div>
+            </form>
+            <div className="todo-container">
+                <ul id="todo-list"></ul>
+            </div>
+        </main>
+    )
+};
