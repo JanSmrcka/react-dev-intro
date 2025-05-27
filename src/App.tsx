88892f385adc325ae9053435adbeaf9cd3a355c3
Trong Dat Luu <david19au@gmail.com>
2025-05-27 13:54:53 +0200

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { Header } from './components/header'
 import { TodosSection } from './components/todos/todos-section'
 import { TodosProvider } from './providers/todos.provider'
+
 function App() {
   return (
     <>
       <TodosProvider>
         <div className="container">
-          <Header title="My Todo List" subtitle="Add your tasks."></Header>
+          <Header title="My Todo List" subtitle="Add your tasks" />
           <TodosSection />
+          <footer>
+            <p>Click on a task to mark it as completed</p>
+          </footer>
         </div>
-        <footer>
-          <p>Click on a task to mark it as completed</p>
-        </footer>
       </TodosProvider>
     </>
   )
