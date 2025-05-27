d4349d85fbce42d49af1b0afc82a3f2dff2bede3
Trong Dat Luu <david19au@gmail.com>
2025-05-27 09:48:10 +0200

    componentarize todo-section, todo-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
+import { Header } from './components/header'
+import { TodosSection } from './components/todos/todos-section'
 function App() {
   return (
     <>
       <div className="container">
-        <header>
-          <h1>My Todo List</h1>
-          <p className="subtitle">Add your tasks</p>
-        </header>
-        <main>
-          <form id="todo-form">
-            <div className="input-group">
-              <input name="todo-text" id="new-todo-input" placeholder="What needs to be done?" />
-              <button type="submit" id="add-btn">
-                Add
-              </button>
-            </div>
-          </form>
-          <div className="todo-container">
-            <ul id="todo-list"></ul>
-          </div>
-        </main>
-        <footer>
-          <p>Click on a task to mark it as completed</p>
-        </footer>
+        <Header title="My Todo List" subtitle="Add your tasks."></Header>
+        <TodosSection />
       </div>
+      <footer>
+        <p>Click on a task to mark it as completed</p>
+      </footer>
     </>
   )
 }
