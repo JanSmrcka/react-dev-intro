dffac905d114d1c2cfc97ecb060aca4ab7456a82
Trong Dat Luu <david19au@gmail.com>
2025-05-27 10:02:59 +0200

    import from older project with api

--- src/components/todos/todo-form.tsx
+++ src/components/todos/todo-form.tsx
@@ -1,10 +1,22 @@
+import { useState, type ChangeEvent } from 'react'
+
 export const TodoForm = () => {
+  const [todoName, setTodoName] = useState('')
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log('Input changed', e.target.value)
+    setTodoName(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    console.log('Submit text:', todoName)
+  }
+
   return (
-    <form id="todo-form">
-      <div className="input-group">
-        <input name="todo-text" placeholder="What needs to be done?" />
-        <button type="submit">Add</button>
-      </div>
-    </form>
+    <div className="input-group">
+      <input value={todoName} onChange={handleInputChange} name="todo-text" placeholder="What needs to be done?" />
+      <button onClick={handleSubmit} type="submit">
+        Add
+      </button>
+    </div>
   )
 }
