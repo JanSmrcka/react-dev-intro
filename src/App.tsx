552576182c7d313fb35bf4ad380207add3a71531
Petr Stipek <stipepet@fit.cvut.cz>
2025-05-27 09:55:01 +0200

    useState hook for form

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,6 @@ function App() {
       </div>
     </>
   )
-}
+};
 
-export default App
+export default App;
--- src/components/todos/todo-form.tsx
+++ src/components/todos/todo-form.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 export const TodoForm = () => {
+    const [todoName, setTodoName] = useState("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTodoName(e.target.value);
+    };
+
     return (
         <form id="todo-form">
             <div className="input-group">
-                <input name="todo-text" id="new-todo-input" placeholder="What needs to be done?" />
+                <input value={todoName} onChange={handleInputChange} name="todo-text" id="new-todo-input" placeholder="What needs to be done?" />
                 <button type="submit" id="add-btn">
                     Add
                 </button>
