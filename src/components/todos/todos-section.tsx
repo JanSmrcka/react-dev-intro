4a22befc8c55f464b74d4b02476857ac931b864a
Jakub Kafka <info@goliathus.net>
2025-05-27 11:29:26 +0200

    Add loading spinner and toggle functionality to Todo items; update TodosSection to manage loading state and integrate Spinner component.

--- src/components/todos/todos-section.tsx
+++ src/components/todos/todos-section.tsx
@@ -3,20 +3,26 @@ import { TodoForm } from './todo-form'
 import { TodoItem } from './todo-item'
 import type { Todo } from '../../types'
 import { todoApi } from '../../api/todoApi'
+import { Spinner } from '../spinner'
 
 export const TodosSection = () => {
   const [todos, setTodos] = useState<Todo[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const fetchTodos = async () => {
+    setIsLoading(true)
     try {
       const data = await todoApi.fetchTodos()
       setTodos(data)
     } catch (error) {
       console.error('Failed to fetch todos:', error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
   const addTodo = async (todoName: string) => {
+    setIsLoading(true)
     try {
       const newTodo = await todoApi.createTodo(todoName)
       setTodos((prevTodos) => {
@@ -24,15 +30,32 @@ export const TodosSection = () => {
       })
     } catch (error) {
       console.error(error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
   const deleteTodo = async (todoId: number) => {
+    setIsLoading(true)
     try {
       await todoApi.deleteTodo(todoId)
       setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId))
     } catch (error) {
       console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const toggleTodo = async (todoId: number, completed: boolean) => {
+    setIsLoading(true)
+    try {
+      const updatedTodo = await todoApi.toggleTodo(todoId, !completed)
+      setTodos((prevTodos) => prevTodos.map((todo) => (todo.id === todoId ? updatedTodo : todo)))
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -46,9 +69,10 @@ export const TodosSection = () => {
       <div className="todo-container">
         <ul id="todo-list">
           {todos.map((todo) => {
-            return <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+            return <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
           })}
         </ul>
+        {isLoading && todos.length === 0 && <Spinner />}
       </div>
     </main>
   )
