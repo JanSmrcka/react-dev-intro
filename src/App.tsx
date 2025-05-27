ad686688b42a992fa13f50914d8db6b2691dcaf8
kuza <kuzn03@vse.cz>
2025-05-27 15:00:24 +0200

    query

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import { TodosProvider } from './providers/todos.providers'
 import { Layout } from './components/layout'
 import TodoListPage from './pages/todo-list.page'
 import TodoDetailPage from './pages/todo-detail'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <Layout>
-      <TodosProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<TodoListPage />} />
-            <Route path="*" element={<div>Not found</div>} />
-            <Route path="/todos/:id" element={<TodoDetailPage />} />
-          </Routes>
-        </BrowserRouter>
-      </TodosProvider>
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <TodosProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<TodoListPage />} />
+              <Route path="*" element={<div>Not found</div>} />
+              <Route path="/todos/:id" element={<TodoDetailPage />} />
+            </Routes>
+          </BrowserRouter>
+        </TodosProvider>
+      </Layout>
+    </QueryClientProvider>
   )
 }
 
