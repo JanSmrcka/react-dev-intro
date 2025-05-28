4d26fc769efa060900a563dab7ddf4bf91395723
adamhanus <kecupwik@gmail.com>
2025-05-28 10:40:54 +0200

    Implement optimistic updates in useTodoDelete hook to enhance user experience; add onMutate for immediate UI updates and prepare for error handling.

--- src/hooks/useTodoDelete.ts
+++ src/hooks/useTodoDelete.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { todoApi } from '../api/todoApi'
+import type { Todo } from '../types'
 
 export const useTodoDelete = () => {
   const queryClient = useQueryClient()
@@ -9,9 +10,19 @@ export const useTodoDelete = () => {
     mutationFn: async (id: number) => {
       return todoApi.deleteTodo(id)
     },
+    onMutate: (id) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todos'])
+      queryClient.setQueryData<Todo[]>(['todos'], (old) => {
+        return old?.filter((todo) => id !== todo.id)
+      })
+      return { previousTodos }
+    },
+
     onSuccess: () => {
       // When i call todoDelete, the system refetches todos like 8 times. The refetch shloud occur just once. TODO: fix.
       queryClient.invalidateQueries({ queryKey: ['todos'] })
     },
+
+    // TODO onError
   })
 }
