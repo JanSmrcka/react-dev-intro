b57d7c67dc0eb752e927008ffa7223fce5898873
adamhanus <kecupwik@gmail.com>
2025-05-28 10:41:03 +0200

    Refactor useTodoToggle hook to implement optimistic updates; modify mutation function to accept TodoToggle type and add onMutate for immediate UI updates, enhancing user experience.

--- src/hooks/useTodoToggle.ts
+++ src/hooks/useTodoToggle.ts
@@ -1,15 +1,24 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { todoApi } from '../api/todoApi'
-import type { Todo } from '../types'
+import type { Todo, TodoToggle } from '../types'
 
 export const useTodoToggle = () => {
   const queryClient = useQueryClient()
 
   return useMutation({
     mutationKey: ['toggleTodo'],
-    mutationFn: async (todo: Todo) => {
-      return todoApi.toggleTodo(todo.id, !todo.completed)
+    mutationFn: async (todoToggle: TodoToggle) => {
+      return todoApi.toggleTodo(todoToggle)
     },
+
+    onMutate: async (todoToggle) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todos'])
+      queryClient.setQueryData<Todo[]>(['todos'], (old) => {
+        return old?.map((todo) => (todo.id === todoToggle.id ? { ...todo, completed: todoToggle.completed } : todo))
+      })
+      return { previousTodos }
+    },
+
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] })
     },
