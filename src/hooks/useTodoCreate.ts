b07753143aaeb43f03128b335d3973a5d4fd441e
Trong Dat Luu <david19au@gmail.com>
2025-05-28 10:00:31 +0200

    refactor to use more hooks

---
+++ src/hooks/useTodoCreate.ts
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { todoApi } from '../api/todoApi'
+
+export const useTodoCreate = () => {
+  const queryClient = useQueryClient()
+  queryClient.invalidateQueries({ queryKey: ['todos'] })
+
+  return useMutation({
+    mutationKey: ['createTodo'],
+    mutationFn: async (todoName: string) => {
+      return await todoApi.createTodo(todoName)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
+    },
+  })
+}
