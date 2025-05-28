aca92207be7a0d698b5fd45b36534c103f48a5ce
adamhanus <kecupwik@gmail.com>
2025-05-28 10:10:43 +0200

    Enhance useTodoCreate hook to include optimistic updates for new todos; improve user experience by immediately reflecting changes in the UI before server confirmation.

--- src/hooks/useTodoCreate.ts
+++ src/hooks/useTodoCreate.ts
@@ -1,14 +1,28 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { todoApi } from '../api/todoApi'
+import { todoApi, ApiError } from '../api/todoApi'
+import type { Todo } from '../types'
 
 export const useTodoCreate = () => {
   const queryClient = useQueryClient()
 
   queryClient.invalidateQueries({ queryKey: ['todos'] })
-  return useMutation({
+  return useMutation<Todo, ApiError, string>({
     mutationKey: ['createTodo'],
     mutationFn: async (todoName: string) => {
       return todoApi.createTodo(todoName)
+    },
+    onMutate: async (todoName) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todos'])
+      queryClient.setQueryData(['todos'], () => {
+        return [
+          ...(previousTodos || []),
+          {
+            name: todoName,
+            id: Date.now(),
+            completed: false,
+          },
+        ]
+      })
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] })
