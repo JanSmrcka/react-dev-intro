46caa55cff677bd481a78230556c746eb1aa91e6
adamhanus <kecupwik@gmail.com>
2025-05-28 09:38:51 +0200

    Enhance useTodoCreate hook to invalidate todos query on success; ensure up-to-date data after creating a new todo.

--- src/hooks/useTodoCreate.ts
+++ src/hooks/useTodoCreate.ts
@@ -1,11 +1,17 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { todoApi } from '../api/todoApi'
 
 export const useTodoCreate = () => {
+  const queryClient = useQueryClient()
+
+  queryClient.invalidateQueries({ queryKey: ['todos'] })
   return useMutation({
     mutationKey: ['createTodo'],
     mutationFn: async (todoName: string) => {
       return todoApi.createTodo(todoName)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
     },
   })
 }
