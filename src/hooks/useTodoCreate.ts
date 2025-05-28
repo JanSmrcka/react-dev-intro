f12b781c07022efb4b6a1f289813e2232b254e3c
Trong Dat Luu <david19au@gmail.com>
2025-05-28 10:10:35 +0200

    caching and better ux by reflecting change to user immediately

--- src/hooks/useTodoCreate.ts
+++ src/hooks/useTodoCreate.ts
@@ -1,14 +1,28 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { todoApi } from '../api/todoApi'
+import { ApiError, todoApi } from '../api/todoApi'
+import type { Todo } from '../types'
 
 export const useTodoCreate = () => {
   const queryClient = useQueryClient()
   queryClient.invalidateQueries({ queryKey: ['todos'] })
 
-  return useMutation({
+  return useMutation<Todo, ApiError, string>({
     mutationKey: ['createTodo'],
     mutationFn: async (todoName: string) => {
       return await todoApi.createTodo(todoName)
+    },
+    onMutate: async (todoName) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todos'])
+      queryClient.setQueryData(['todos'], () => {
+        return [
+          ...(previousTodos || []),
+          {
+            name: todoName,
+            id: Date.now(),
+            completed: false,
+          },
+        ]
+      })
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] })
