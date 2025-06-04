6b966033b77c3e855d24e83691d0825444008837
adamhanus <kecupwik@gmail.com>
2025-06-04 22:19:04 +0200

    refactor: remove TodosProvider and related context/hooks; simplify App component structure by directly integrating routing since QueryClient is used

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import { TodosProvider } from './providers/todos.provider'
 import { Layout } from './components/layout'
 import { lazy, Suspense } from 'react'
 import { Spinner } from './components/spinner'
@@ -17,29 +16,27 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
-        <TodosProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <Suspense fallback={<Spinner />}>
-                    <TodoListPage />
-                  </Suspense>
-                }
-              />
-              <Route
-                path="/todos/:id"
-                element={
-                  <Suspense fallback={<Spinner />}>
-                    <TodoDetailPage />
-                  </Suspense>
-                }
-              />
-              <Route path="*" element={<div>Not found</div>} />
-            </Routes>
-          </BrowserRouter>
-        </TodosProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <TodoListPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/todos/:id"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <TodoDetailPage />
+                </Suspense>
+              }
+            />
+            <Route path="*" element={<div>Not found</div>} />
+          </Routes>
+        </BrowserRouter>
       </Layout>
     </QueryClientProvider>
   )
